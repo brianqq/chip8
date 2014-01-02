b4572bb7010859b5c41d8bfe4ed1c54b4b533f97
Brian Levy <brianlevy95@gmail.com>
2014-01-01 20:22:42 -0500

    added readme

--- chip8.lisp
+++ chip8.lisp
@@ -1,6 +1,5 @@
-(declaim (optimize debug safety))
-(deftype chipbyte ()
-  '(unsigned-byte 8))
+(declaim (optimize (debug 3) (safety 3)))
+(deftype chipbyte () '(unsigned-byte 8))
 (defun make-byte-array (size &rest extras)
   (apply #'make-array size :element-type 'chipbyte :adjustable nil extras))
 (declaim (inline make-byte-array))
@@ -14,7 +13,7 @@
 (defparameter *registers*
   (make-byte-array 16))
 (defparameter *index* 0)
-(defparameter *pc* *startram*) 			;program counter
+(defparameter *pc* +startram+) 			;program counter
 (defparameter *gfx*
   (make-byte-array '(64 32)))
 (defparameter *gfx-flat*
@@ -43,7 +42,11 @@
 (define-symbol-macro V0 (aref *registers* 0))
 (define-symbol-macro stacktop (aref *stack* *sp*))
 (defun next () (incf *pc* 2))
-(declaim (optimize step))
+(declaim (inline next))
+
+;; (defmacro ssetf (arg value)
+;;   `(symbol-macrolet ((it ,arg))
+;;      (setf it ,value)))
 
 ;;; run away it's the opcode table thing
 (defparameter *opcodes*
@@ -82,22 +85,22 @@
 	  (setf VX (logxor VX VY)))
 	 ("8XY4" 	"Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't."
 	  (let ((sum (+ VY VX)))
-	    (setf VX (mod (+ VY VX) *max-val*))
-	    (setf VF (if (> sum *max-val*) 1 0))))
+	    (setf VX (mod (+ VY VX) *max-val*)
+		  VF (if (> sum *max-val*) 1 0))))
 	 ("8XY5" 	"VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't."
 	  (let ((diff (- VX VY)))
-	    (setf VX (mod (+ VY VX) *max-val*))
-	    (setf VF (if (< diff 0) 0 1))))
+	    (setf VX (mod (+ VY VX) *max-val*)
+		  VF (if (< diff 0) 0 1))))
 	 ("8XY6" 	"Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.[2]"
 	  (multiple-value-bind (div rem) (truncate VX 2)
 	    (setf VX div VF rem)))
 	 ("8XY7" 	"Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't."
 	  (let ((diff (- VY VX)))
-	    (setf VX (mod (+ VY VX) *max-val*))
-	    (setf VF (if (< diff 0) 0 1))))
+	    (setf VX (mod (+ VY VX) *max-val*)
+		  VF (if (< diff 0) 0 1))))
 	 ("8XYE" 	"Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.[2]"
-	  (setf VF (ash VX -7))
-	  (setf VX (ash VX 1)))
+	  (setf VF (ash VX -7)
+	   VX (ash VX 1)))
 	 ("9XY0" 	"Skips the next instruction if VX doesn't equal VY."
 	  (if (/= VX VY) (next)))
 	 ("ANNN" 	"Sets I to the address NNN."
@@ -107,7 +110,17 @@
 	  (setf *pc* (+ N V0)))
 	 ("CXNN" 	"Sets VX to a random number and NN."
 	  (logand (random #xFF) N))
-	 ("DXYN" 	"Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded (with the most significant bit of each byte displayed on the left) starting from memory location I; I value doesn't change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn't happen.")
+	 ("DXYN" 	"Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded (with the most significant bit of each byte displayed on the left) starting from memory location I; I value doesn't change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn't happen."
+	  (setf VF 0)
+	  (loop for left from 0 below 8 
+	     with xdest = (+ left VX) do
+	       (loop for down from 0 below N
+		  with ydest = (+ down VY) 
+		  with pixel = (logand (ash (aref *memory* (+ *index* down)) (- xdest)) 1)
+		  do (symbol-macrolet ((it (aref *gfx* xdest ydest)))
+		       (setf it (logxor it pixel))
+		       (if (/= (logand it pixel) 0)
+			   (setf VF 1))))))
 	 ("EX9E" 	"Skips the next instruction if the key stored in VX is pressed."
 	  (if (pressedp (aref *key* VX)) (next)))
 	 ("EXA1" 	"Skips the next instruction if the key stored in VX isn't pressed."
@@ -146,14 +159,13 @@
 		     ,(map 'string (lambda (x) (if (char= x #\N) #\F #\0)) opcode)))
 	       `(lambda (opcode)
 		  (and (= ,match (logior opcode ,(logior ymask (logior nmask xmask))))
-		       (let ((X (ash (logand ,xmask opcode) -8))
-			     (Y (ash (logand ,ymask opcode) -4))
-			     (N (logand ,nmask opcode)))
-			 (declare (ignorable X Y N))
-			 ,@(destructuring-bind (car &rest cdr) (or body (list nil nil))
-					       (if (eq car 'holdpc)
-						   cdr
-						   (append body
-							   (list '(next)))))))))))
-
+		       (or (let ((X (ash (logand ,xmask opcode) -8))
+			      (Y (ash (logand ,ymask opcode) -4))
+			      (N (logand ,nmask opcode)))
+			     (declare (ignorable X Y N))
+			     ,@(if (eq (car body) 'holdpc)
+				   (cdr body)
+				   (append body
+					   (list '(next)))))
+			   t))))))
 
